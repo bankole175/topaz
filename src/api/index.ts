35c0baa16e75fbf22ba6dd7cb51bcbce34d25f92
bhanky <samuel.bankole@quabbly.com>
2022-09-21 07:19:40 +0100

    add axios

---
+++ src/api/index.ts
@@ -0,0 +1,36 @@
+import Axios from 'axios'
+
+export default class {
+  static post(uri: string, data: any) {
+    return this.http().post(this.setUri(uri), data)
+  }
+
+  static put(uri: string, data: any) {
+    return this.http().patch(this.setUri(uri), data)
+  }
+
+  static get(uri: string) {
+    return this.http().get(this.setUri(uri))
+  }
+
+  static patch(uri: string, data: any) {
+    return this.http().patch(this.setUri(uri), data)
+  }
+
+  static delete(uri: string) {
+    return this.http().delete(this.setUri(uri))
+  }
+
+  static http() {
+    return Axios.create({
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_APP_BEARER_TOKEN}`,
+        Accept: 'application/json',
+      },
+    })
+  }
+
+  static setUri(uri: string) {
+    return import.meta.env.VITE_APP_BASE_URL + uri
+  }
+}
